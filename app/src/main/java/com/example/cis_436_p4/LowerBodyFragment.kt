abecaf7123b4032e76d46cf6189fb86cf14bde85
Ryan Len <ryan.len@logicdrop.com>
2024-04-21 21:16:36 -0400

    Scroll view in lower body fragment to store logged activities

--- app/src/main/java/com/example/cis_436_p4/LowerBodyFragment.kt
+++ app/src/main/java/com/example/cis_436_p4/LowerBodyFragment.kt
@@ -5,21 +5,60 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.ArrayAdapter
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
+import com.example.cis_436_p4.databinding.FragmentLowerBodyBinding
 
 class LowerBodyFragment : Fragment() {
+
+    private lateinit var binding: FragmentLowerBodyBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // to go from lower body to home
-        val view = inflater.inflate(R.layout.fragment_lower_body, container, false)
-        val goHomeButton = view.findViewById<Button>(R.id.btnLowerHome)
-        goHomeButton.setOnClickListener{
+        binding = FragmentLowerBodyBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        setupViews()
+        setupListeners()
+
+        return view
+    }
+
+    private fun setupViews() {
+        val exercises = arrayOf("Squats into Jumps", "Calf Raises", "Lunges")
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, exercises)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.exerciseSpinner.adapter = adapter
+    }
+
+    private fun setupListeners() {
+        binding.btnLogActivity.setOnClickListener {
+            logActivity()
+        }
+
+        binding.btnLowerHome.setOnClickListener {
             findNavController().navigate(R.id.action_lowerBodyFragment_to_homeFragment)
         }
-        return view
+    }
+    private fun logActivity() {
+        val date = binding.etDate.text.toString()
+        val exercise = binding.exerciseSpinner.selectedItem?.toString() ?: ""
+        val weight = binding.etWeight.text.toString()
+        val reps = binding.etReps.text.toString()
+
+        // Create a new TextView to display the logged activity
+        val loggedActivityTextView = TextView(context)
+        loggedActivityTextView.text = "Date: $date, Exercise: $exercise, Weight: $weight, Reps: $reps"
+
+        // Add the TextView to the LinearLayout inside ScrollView
+        binding.llLoggedActivities.addView(loggedActivityTextView)
+
+        // Clear input fields after logging
+        binding.etDate.text.clear()
+        binding.etWeight.text.clear()
+        binding.etReps.text.clear()
     }
 }
